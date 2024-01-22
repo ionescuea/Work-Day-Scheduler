a1f55ddf95ecf39983fa21d23268a5f048b9ddee
ionescuea <ionescuea@yahoo.com>
2024-01-22 22:37:30 +0000

    Get items from local storage; styling

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
-// Display current day
 var nowTime = dayjs(new Date());
+
+// Display current day
 var now = dayjs(new Date()).format("dddd, D MMMM YYYY");
 $("#currentDay").text(now);
 
-
+// Color-code depending on current time
 $(".description").each(function() {
     var blockTime = dayjs(new Date()).set("hour", this.id);
     if (blockTime < nowTime) {
@@ -15,6 +16,12 @@ $(".description").each(function() {
     }
 });
 
+// Get items from local storage
+$(".description").each(function(){
+    $(this).text(localStorage.getItem(this.id))
+})
+
+// Save to local storage
 $(":button").on("click", function() {
     var text = $(this).prev("textarea");
     localStorage.setItem(text[0].id, text.val())
