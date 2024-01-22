270eabfe71568796fa0e2dde515942ac41dec1d5
ionescuea <ionescuea@yahoo.com>
2024-01-22 18:57:38 +0000

    Add color-code to timeblocks

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,16 @@
 // Display current day
+var nowTime = dayjs(new Date());
 var now = dayjs(new Date()).format("dddd, D MMMM YYYY");
 $("#currentDay").text(now);
 
+
+$(".description").each(function() {
+    var blockTime = dayjs(new Date()).set("hour", this.id);
+    if (blockTime < nowTime) {
+        $(this).addClass("past");
+    } else if (blockTime.get("hour") <= nowTime.get("hour") && nowTime.get("hour") < blockTime.get("hour") + 1) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("future");
+    }
+});
